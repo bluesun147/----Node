cb7c00d2b0421272cf3ee3d66fc570c35363985f
HaeChan Kim <bluesun147@naver.com>
2022-03-14 19:54:44 +0900

    first commit

---
+++ condition.js
@@ -0,0 +1,11 @@
+const args = process.argv;
+console.log(args);
+console.log(args[2]);
+console.log('a');
+console.log('b');
+if (args[2] === '1') {
+    console.log('c1');
+} else {
+    console.log('c2');
+}
+console.log('d');
